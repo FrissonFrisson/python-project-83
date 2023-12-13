75d4fadb0387fa805566c44f25bdae0313d69b32
FrissonFrisson <compton56rus@gmail.com>
2023-12-14 01:01:59 +0300

    add routing

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,17 +1,55 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, url_for, flash, abort, redirect
+import os
+from page_analyzer.url_handler import validate_url, normalize_urls
+from page_analyzer import db
+from dotenv import load_dotenv
 
+SECRET_KEY = os.getenv('SECRET_KEY')
 app = Flask(__name__)
+app.secret_key = SECRET_KEY
 
 
 @app.route('/')
 def index_page():
-    return render_template(
-        'index.html'
-    )
+    return render_template('index.html')
 
 
 @app.route('/urls', methods=['POST'])
 def analyze_url():
+    conn = db.connect()
     url = request.form['url']
-    # здесь можно добавить логику для анализа URL
-    return render_template('urls.html', url=url)
+    validate = validate_url(url)
+    if validate['status'] is False:
+        flash(*validate['msg'])
+        return render_template('index.html')
+    url = normalize_urls(url)
+
+    if db.check_in_urls(url):
+        id = db.get_id_url(url)
+        flash('Страница уже существует','info')
+        return redirect(url_for('get_page_url', id = id))
+    else:
+        db.add_url(url)
+        id = db.get_id_url(url)
+        flash('Страница успешно добавлена','success')
+        return redirect(url_for('get_page_url', id = id))
+
+
+@app.route('/urls/<id>')
+def get_page_url(id):
+    url_info = db.get_url_info(id)
+    if not url_info:
+        return abort(404)
+    id, name, date = url_info
+    return render_template('page_url.html', id=id, name=name, date=date)
+
+
+@app.get('/urls')
+def get_page_urls():
+    list_urls = db.get_list_urls()
+    return render_template('urls.html', urls = list_urls)
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html'), 404
---
+++ page_analyzer/db.py
@@ -0,0 +1,61 @@
+import psycopg2
+import os
+import datetime
+from dotenv import load_dotenv
+
+
+load_dotenv()
+DATABASE_URL = os.getenv('DATABASE_URL')
+
+
+def connect():
+    try:
+        # пытаемся подключиться к базе данных
+        conn = psycopg2.connect(DATABASE_URL)
+        cur = conn.cursor()
+        return conn, cur
+    except:
+        # в случае сбоя подключения будет выведено сообщение  в STDOUT
+        print('Can`t establish connection to database')
+
+
+def check_in_urls(url):
+    conn, cur = connect()
+    cur.execute('SELECT urls FROM urls WHERE name = %s', (url,))
+    rows = cur.fetchall()
+    conn.close()
+    if rows:
+        return True
+    else:
+        return False
+    
+
+def add_url(url):
+    conn, cur = connect()
+    cur.execute('INSERT INTO urls (name, created_at) VALUES (%s, %s)', (url,datetime.datetime.now()))
+    conn.commit()
+    conn.close()
+
+
+def get_url_info(id):
+    conn, cur = connect()
+    cur.execute('SELECT id, name, created_at FROM urls WHERE id = CONVERT(bigint, %s)', (id,))
+    rows = cur.fetchone()
+    conn.close()
+    return rows
+
+
+def get_id_url(url):
+    conn, cur = connect()
+    cur.execute('SELECT id FROM urls WHERE name = %s', (url,))
+    rows = cur.fetchone()
+    conn.close()
+    return rows[0]
+
+
+def get_list_urls():
+    conn, cur = connect()
+    cur.execute('SELECT id, name, created_at FROM urls')
+    rows = cur.fetchall()
+    conn.close()
+    return rows
